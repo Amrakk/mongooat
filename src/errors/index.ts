158d0458aa70f4ab612925666d5f78eb19220628
Amrakk <4mr4kk@gmail.com>
2024-10-06 20:20:39 +0700

    feat: release v2.2.5
    feat: modify export for better UX

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,4 @@
-import ModelError from "./model/index.js";
+export * from "./model/index.js";
 
 import DBNotSetError from "./dbNotSet.js";
 import MongooatError from "./mongooatError.js";
@@ -7,14 +7,4 @@ import ModelExistedError from "./modelExisted.js";
 import MethodExistedError from "./methodExisted.js";
 import MethodNotFoundError from "./methodNotFound.js";
 
-const MGErrors = {
-    ModelError,
-    DBNotSetError,
-    MongooatError,
-    ValidationError,
-    ModelExistedError,
-    MethodExistedError,
-    MethodNotFoundError,
-};
-
-export default MGErrors;
+export { DBNotSetError, MongooatError, ValidationError, ModelExistedError, MethodExistedError, MethodNotFoundError };
