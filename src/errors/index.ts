d5408e0c30eb845eb50e5a244896c6e2ad77eebb
Amrakk <4mr4kk@gmail.com>
2024-10-25 13:21:34 +0700

    feat: release v2.2.18
    fix: rename import validateError

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,9 +2,9 @@ export * from "./model/index.js";
 
 import DBNotSetError from "./dbNotSet.js";
 import MongooatError from "./mongooatError.js";
-import ValidationError from "./validateError.js";
+import ValidateError from "./validateError.js";
 import ModelExistedError from "./modelExisted.js";
 import MethodExistedError from "./methodExisted.js";
 import MethodNotFoundError from "./methodNotFound.js";
 
-export { DBNotSetError, MongooatError, ValidationError, ModelExistedError, MethodExistedError, MethodNotFoundError };
+export { DBNotSetError, MongooatError, ValidateError, ModelExistedError, MethodExistedError, MethodNotFoundError };
